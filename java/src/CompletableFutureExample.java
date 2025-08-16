4169fe1461d50324a2174be8788ba5e88d877d92
Shivani Singh Bairwa <76783378+singh3003@users.noreply.github.com>
2025-08-16 16:39:24 +0530

    "adding completable future example files"

---
+++ java/src/CompletableFutureExample.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.CompletableFuture;
+
+public class CompletableFutureExample {
+
+
+    public static void main(String[] args) {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> 10).thenApply(
+                result -> result * 2).thenApply(
+                        result -> String.valueOf(result + 5)
+        );
+
+        future1.thenAccept(System.out::println);
+
+    }
+}
